c99f9467f38b4ef12bc04005a5f94b8faa0ffb6c
serge <dev@rfbus.ru>
2023-08-24 20:30:39 +1000

    leftovers...

---
+++ bloodwave-rs/src/main.rs
@@ -0,0 +1,26 @@
+use std::io::Cursor;
+use rand::{thread_rng, Rng};
+use rodio::Sink;
+use rodio::{Decoder, OutputStream};
+
+mod wavs;
+
+fn main() {
+
+    let files = wavs::main();
+
+    let mut rng = thread_rng();
+    let position = rng.gen_range(0..files.len() as u32) as usize;
+    // println!("pos {} of {}", position, files.len());
+    // println!("selected {:?}", files[position]);
+
+    // Get a output stream handle to the default physical sound device
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&stream_handle).unwrap();
+
+    let data = files[position];
+    let wave = data.contents();
+    let source = Decoder::new(Cursor::new(wave)).unwrap();
+    sink.append(source);
+    sink.sleep_until_end();
+}
